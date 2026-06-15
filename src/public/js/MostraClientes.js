b527af144c15189acf757df89a5a6a44e69ea924
Jordi_gumbert <jordigumbert@gmail.com>
2026-06-15 11:37:16 +0200

    esquelet primera funcio ùblic views

---
+++ src/public/js/MostraClientes.js
@@ -0,0 +1,36 @@
+// module.exports = {
+//     getAllClients,
+//     getClientById,
+//     createClient,
+//     updateClient,
+//     deleteClient
+// };
+
+// async function getAllClients(req, res) {
+//     try {
+//         const clients = await clientRepository.getAll();
+
+//         res.status(200).json(clients);
+//     } catch (error) {
+//         console.error(error);
+
+//         res.status(500).json({
+//             message: "Error obtenint clients"
+//         });
+//     }
+// }
+
+
+const path = require("path"); // par also requiere seria lo suyo usarlo... 
+
+//const  = require("../repositories/client");
+
+
+async function clientViewFilter ( ) {
+
+
+}; 
+
+module.exports = {
+    clientViewFilter ,
+}
