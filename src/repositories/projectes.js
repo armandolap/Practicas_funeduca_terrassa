63f8004132c0027ffdd414cbde0e529ebf387b92
Angel <angelpsan97@gmail.com>
2026-06-15 14:02:51 +0200

    modificacion projectes y usuaris con los ultimos campos de la BBDD,falta sus controladores

--- src/repositories/projectes.js
+++ src/repositories/projectes.js
@@ -1,15 +1,15 @@
 const { createPool } = require("../config/database");
 
 const pool = createPool();
-// aqui v ala peticion al server SQL 
+// aqui va la peticion al server SQL 
 // FALTEN PUT i POST per editar i crear
 
 async function getAll() {
     const [rows] = await pool.query(`
-    SELECT *
-    FROM Proyectos
-    ORDER BY Nom_projecte
-  `);
+        SELECT *
+        FROM proyectos
+        ORDER BY Nom_projecte
+    `);
 
     return rows;
 }
@@ -17,73 +17,60 @@ async function getAll() {
 async function getById(id) {
     const [rows] = await pool.query(
         `
-    SELECT *
-    FROM Proyectos
-    WHERE idProyecto = ?
-    `,
+        SELECT *
+        FROM proyectos
+        WHERE idProyecto = ?
+        `,
         [id]
     );
 
     return rows[0] || null;
 }
 
-
-
 async function create(projecteData) {
     // Desestructuramos para mayor claridad y asignamos valores por defecto de BBDD si es necesario
     const {
         Nom_projecte,
         Descripcio,
-        responsable,
-        Centro_coord = 1, // Valor por defecto a nivel de BBDD si no viene
         fecha_inicio,
-        fecha_fin,
-        //ubicacion,
+        fecha_fin = null,
         plazas = 0,
         inscritos = 0,
-        fecha_inicio_act,
-        fecha_fin_act
+        fecha_inicio_act = null,
+        fecha_fin_act = null,
+        idcentre_activitats
     } = projecteData;
 
-    // Consulta parametrizada. El orden de las '?' debe coincidir EXACTAMENTE con el array 'values'
     const query = `
-        INSERT INTO Proyectos (
-            Nom_projecte, 
-            Descripcio, 
-            responsable, 
-            Centre_coordinacio, 
-            fecha_inicio, 
-            fecha_fin, 
-            plazas, 
-            inscritos, 
-            fecha_inicio_act, 
-            fecha_fin_act
-            -- ubicacion,
-        ) VALUES (?, ?, ?, ?, ?, ?,  ?, ?, ?, ?)
-    `; // cuidado que els 7o ? se ha quitado para no meter la ubicacion
+        INSERT INTO proyectos (
+            Nom_projecte,
+            Descripcio,
+            fecha_inicio,
+            fecha_fin,
+            plazas,
+            inscritos,
+            fecha_inicio_act,
+            fecha_fin_act,
+            idcentre_activitats
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
 
     const values = [
         Nom_projecte,
         Descripcio,
-        responsable,
-        Centro_coord,
         fecha_inicio,
         fecha_fin,
         plazas,
         inscritos,
         fecha_inicio_act,
         fecha_fin_act,
-        //ubicacion,
+        idcentre_activitats
     ];
 
-    // Ejecutamos la query. 'result' contendrá { affectedRows: 1, insertId: 42, ... }
     const [result] = await pool.query(query, values);
-    
+
     return result.insertId;
 }
-
-
-
 
 module.exports = {
     getAll,
